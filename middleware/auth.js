8104c5d110cce2ba436b5758ae33e2080182055d
Pelin duman <pelindmn04@gmail.com>
2025-11-30 19:29:13 +0300

    auth.js updated for debug

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,17 @@
-const jwt = require("jsonwebtoken");
-
 module.exports = function (req, res, next) {
-const authHeader = req.headers['authorization'];
+
+    console.log("===== AUTH DEBUG START =====");
+    console.log("AUTH HEADER RECEIVED RAW:", req.headers);
+    console.log("AUTH HEADER RECEIVED AUTH ONLY:", req.headers["authorization"]);
+    console.log("===== AUTH DEBUG END =====");
+
+    const authHeader = req.headers['authorization'];
 
     if (!authHeader) {
         return res.status(401).json({ error: "Authorization header missing" });
     }
 
-    const token = authHeader.split(" ")[1]; 
+    const token = authHeader.split(" ")[1];
 
     if (!token) {
         return res.status(401).json({ error: "Token missing" });
@@ -15,8 +19,8 @@ const authHeader = req.headers['authorization'];
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;  
-        next(); 
+        req.user = decoded;
+        next();
     } catch (error) {
         return res.status(403).json({ error: "Invalid or expired token" });
     }

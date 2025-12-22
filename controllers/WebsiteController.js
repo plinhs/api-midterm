476414ba49a4f1e49e8d13efcb348aa9d1679147
Pelin duman <pelindmn04@gmail.com>
2025-12-22 15:58:35 +0300

    typo error

--- controllers/WebsiteController.js
+++ controllers/WebsiteController.js
@@ -7,7 +7,7 @@ class WebsiteController{
 
     async payBill(req, res){
         const { subscriber_no, month, amount } = req.body;
-        const payment = await this.paymentService.payBills(subscriber_no, month, amount);
+        const payment = await this.paymentService.payBill(subscriber_no, month, amount);
         if (payment.error) return res.status(404).json(payment);
         res.status(200).json(payment);
     }
